f48247c36d8d9349820313cf804fb30b80f3b35f
“Gisela-Soledad-Hurtado” <hurtadogiselasoledad@gmail.com>
2024-05-21 08:25:48 +0200

    add exercise1 and 2

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import { nombre } from "./js/ex1.js";
 
-function app() {
-    console.log("El nombre es : " + nombre);
+//let numb1 = 8;
+//let numb2 = 2;
+
+
+function app(param1, param2) {
+    //console.log("El nombre es : " + nombre);
+    
+    return param1 + param2;
 }
 
 app()
+
+console.log(app(4, 5));
