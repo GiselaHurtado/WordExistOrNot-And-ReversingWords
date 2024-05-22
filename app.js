eadd24be34ee137e014f0c1b3e7495d19fb7c315
“Gisela-Soledad-Hurtado” <hurtadogiselasoledad@gmail.com>
2024-05-22 11:31:45 +0200

    ex4 return

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import {  } from "./js/ex3.js";
+import {  } from "./js/ex4.js";
 
 function app(){
 
---
+++ js/ex4.js
@@ -0,0 +1,16 @@
+function reverseWords(str) {
+    // Recortar espacios innecesarios y dividir la cadena en palabras
+    let words = str.trim().split(' ');
+
+    // Invertir el arreglo de palabras y unirlo en una cadena
+    let reversedStr = words.reverse().join(' ');
+
+    return reversedStr;
+}
+
+// Ejemplos de uso
+let inputString1 = "Hello World";
+let inputString2 = "Hi There.";
+
+console.log(reverseWords(inputString1)); // Debería imprimir "World Hello"
+console.log(reverseWords(inputString2)); // Debería imprimir "There. Hi"
