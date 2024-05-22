a080c0b8b0be02d007d787b250d3afbc34a52086
“Gisela-Soledad-Hurtado” <hurtadogiselasoledad@gmail.com>
2024-05-22 11:08:02 +0200

    ex4 reverseword

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-import { nombre } from "./js/ex1.js";
+import {  } from "./js/ex3.js";
 
-//let numb1 = 8;
-//let numb2 = 2;
+function app(){
 
-
-function app(param1, param2) {
-    //console.log("El nombre es : " + nombre);
-    
-    return param1 + param2;
 }
 
 app()
-
-console.log(app(4, 5));
